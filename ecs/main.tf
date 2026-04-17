454fe9f5786e84186ebd55f3b9622589de1002d6
Ghostwritten <1zoxun1@gmail.com>
2026-04-17 15:20:03 +0800

    init terraform demo

--- ecs/main.tf
+++ ecs/main.tf
@@ -16,30 +16,30 @@ provider "alicloud" {
 data "alicloud_instance_types" "demo" {
   availability_zone = "cn-hangzhou-b"
   cpu_core_count    = 2
-  memory_size       = 4
+  memory_size       = 2
+}
+
+variable "password" {
+  type      = string
+  sensitive = true
 }
 
 resource "alicloud_instance" "demo" {
-  instance_name = "tf-demo-ecs"
+  instance_name = "tf-demo-ecs-mini"
 
   instance_type     = data.alicloud_instance_types.demo.instance_types[0].id
   availability_zone = "cn-hangzhou-b"
 
   image_id = "aliyun_3_x64_20G_alibase_20260326.vhd"
 
-  system_disk_category = "cloud_essd"
-  system_disk_size     = 40
+  system_disk_category = "cloud_efficiency"
+  system_disk_size     = 20
 
   vswitch_id      = "vsw-bp1fd12ephv5gskixctys"
   security_groups = ["sg-bp1ft576ohr29b383u6n"]
 
-  internet_charge_type      = "PayByTraffic"
-  internet_max_bandwidth_out = 1
+  internet_charge_type       = "PayByTraffic"
+  internet_max_bandwidth_out = 0
 
   password = var.password
-}
-
-variable "password" {
-  type      = string
-  sensitive = true
 }
