c20b751371bdf0fddbf126783980173974185c87
Ghostwritten <1zoxun1@gmail.com>
2026-04-17 15:27:02 +0800

    init terraform demo

--- ecs/main.tf
+++ ecs/main.tf
@@ -24,22 +24,23 @@ variable "password" {
   sensitive = true
 }
 
+
 resource "alicloud_instance" "demo" {
   instance_name = "tf-demo-ecs-mini"
 
-  instance_type     = data.alicloud_instance_types.demo.instance_types[0].id
+  instance_type     = "ecs.e-c1m1.large"
   availability_zone = "cn-hangzhou-b"
 
   image_id = "aliyun_3_x64_20G_alibase_20260326.vhd"
 
-  system_disk_category = "cloud_efficiency"
+  system_disk_category = "cloud_essd"
   system_disk_size     = 20
 
   vswitch_id      = "vsw-bp1fd12ephv5gskixctys"
   security_groups = ["sg-bp1ft576ohr29b383u6n"]
 
   internet_charge_type       = "PayByTraffic"
-  internet_max_bandwidth_out = 0
+  internet_max_bandwidth_out = 1
 
   password = var.password
 }
