ba47ba0542600af0f20ad0012518e557946cc7ec
Ghostwritten <1zoxun1@gmail.com>
2026-04-17 14:41:49 +0800

    init terraform demo

--- ecs/main.tf
+++ ecs/main.tf
@@ -1,21 +1,23 @@
 data "alicloud_images" "alinux" {
-  name_regex  = "Alibaba Cloud Linux 3.*"
-  most_recent = true
   owners      = "system"
+  most_recent = true
+  name_regex  = ".*Alibaba.*Linux.*"
+}
+
+locals {
+  selected_image_id = try(data.alicloud_images.alinux.images[0].id, null)
 }
 
 resource "alicloud_instance" "demo" {
   instance_name     = "tf-demo-ecs"
-  instance_type     = "ecs.c6.large"
+  instance_type     = "ecs.c9i.large"
   availability_zone = "cn-hangzhou-b"
 
-  image_id = data.alicloud_images.alinux.images[0].id
+  image_id = local.selected_image_id
 
-  system_disk_category       = "cloud_efficiency"
-  system_disk_size           = 20
+  system_disk_category       = "cloud_essd"
+  system_disk_size           = 40
   vswitch_id                 = "vsw-bp1fd12ephv5gskixctys"
   security_groups            = ["sg-bp1ft576ohr29b383u6n"]
   internet_max_bandwidth_out = 1
-
-  password = "YourPassword123!"
 }
